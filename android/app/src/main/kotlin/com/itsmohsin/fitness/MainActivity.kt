f77c5b52aae34f4147a6b98f9a240f66ce3d57fc
Mohsin Khan <mohsinsmannat@gmail.com>
2023-04-14 15:17:59 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/itsmohsin/fitness/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.itsmohsin.fitness
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        // to remove shadow
+        elevation: 0,
+      ),
+    );
+  }
+}
